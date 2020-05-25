c11b868720664c3111131cee3a0c4fe08fa6d280
TheStonedTurtle <mgoodwin559@gmail.com>
2020-05-24 18:11:15 -0700

    Crab Solver - Display Crystal answers

--- src/main/java/thestonedturtle/crabsolver/CrabSolverPlugin.java
+++ src/main/java/thestonedturtle/crabsolver/CrabSolverPlugin.java
@@ -1,24 +1,80 @@
 package thestonedturtle.crabsolver;
 
 import com.google.inject.Provides;
+import java.util.HashMap;
+import java.util.Map;
 import javax.inject.Inject;
-import lombok.extern.slf4j.Slf4j;
+import lombok.Getter;
+import net.runelite.api.Client;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.events.GameObjectDespawned;
+import net.runelite.api.events.GameObjectSpawned;
 import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
 
-@Slf4j
 @PluginDescriptor(
 	name = "Crab Solver"
 )
 public class CrabSolverPlugin extends Plugin
 {
 	@Inject
+	private CrabSolverOverlay overlay;
+
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
 	private CrabSolverConfig config;
+
+	@Inject
+	private Client client;
 
 	@Provides
 	CrabSolverConfig provideConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(CrabSolverConfig.class);
+	}
+
+	@Getter
+	private final Map<CrabCrystal, LocalPoint> crystalMap = new HashMap<>();
+
+	@Override
+	public void startUp()
+	{
+		overlayManager.add(overlay);
+	}
+
+	@Override
+	public void shutDown()
+	{
+		crystalMap.clear();
+		overlayManager.remove(overlay);
+	}
+
+	@Subscribe
+	public void onGameObjectSpawned(GameObjectSpawned e)
+	{
+		final CrabCrystal crystal = CrabCrystal.getByObjectID(e.getGameObject().getId());
+		if (crystal == null)
+		{
+			return;
+		}
+
+		crystalMap.put(crystal, e.getGameObject().getLocalLocation());
+	}
+
+	@Subscribe
+	public void onGameObjectDespawned(GameObjectDespawned e)
+	{
+		final CrabCrystal crystal = CrabCrystal.getByObjectID(e.getGameObject().getId());
+		if (crystal == null)
+		{
+			return;
+		}
+
+		crystalMap.remove(crystal);
 	}
 }
